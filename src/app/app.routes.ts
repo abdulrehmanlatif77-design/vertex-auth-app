3e7f16965b89a4f5ef9f7cf7735b1869f190d416
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-12-27 05:46:41 +0530

    Added the layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { HomeComponent } from './components/home/home';
 import { authGuard } from './guards/auth-guard';
+import { LayoutComponent } from './components/layout/layout';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  },
   { path: '**', redirectTo: 'login' }
 ];
