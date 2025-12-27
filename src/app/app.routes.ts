32fc054c8b0866918b97f8d03fb09af4dccd381e
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-12-27 05:53:23 +0530

    Updated the navbar in the layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,19 @@ import { authGuard } from './guards/auth-guard';
 import { LayoutComponent } from './components/layout/layout';
 
 export const routes: Routes = [
+  // Public route without layout
+  { path: 'login', component: LoginComponent },
+
+  // Private shell with layout
   {
     path: '',
     component: LayoutComponent,
     children: [
-      { path: 'login', component: LoginComponent },
       { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-      { path: '', redirectTo: 'login', pathMatch: 'full' }
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
     ]
   },
+
+  // Fallback
   { path: '**', redirectTo: 'login' }
 ];
